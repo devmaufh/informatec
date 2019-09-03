6f2c5e39f3c8da4b8a14d13ba094f66387d859d4
Devmaufh <mau1361317@gmail.com>
2019-09-03 07:55:56 -0500

    Initial Commit

---
+++ informaTEC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  informaTEC
+//
+//  Created by Labdesarrollo6 on 9/3/19.
+//  Copyright © 2019 Roqbyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
